3f5d24a6d710cf2da33fc7fb023b6f7a06521113
albertoochoa <83051158+albertoochoa@users.noreply.github.com>
2024-12-31 11:40:30 -0500

    Delete handlers/getAllPokemonsScript.py
    File is not working yet

--- handlers/getAllPokemonsScript.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-myclient = pymongo.MongoClient("mongodb+srv://testUser:eQKQHtYpoE0nQlbs@cluster0.jehzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-mydb = myclient["db_pokemons"]
-mycol = mydb["pokemon_data"]
-
-mydict = { "name": "John", "address": "Highway 37" }
-
-x = mycol.insert_one(mydict)
