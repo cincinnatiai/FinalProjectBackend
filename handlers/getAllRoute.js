09de0d0094532f5babadf71e05c8885544505996
GenericDevGeorgia <genericdevgeorgia@Mac.attlocal.net>
2025-01-10 12:42:52 -0500

    improvement to saveAllPokemons

--- handlers/getAllRoute.js
+++ handlers/getAllRoute.js
@@ -3,43 +3,55 @@ const PokemonData = require('../models/pokemons');
 const saveAllPokemonsHandler = async (request, h) => {
   try {
     const savePokemon = async (id) => {
-      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+      try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+        if (!response.ok) {
+          console.error(`Failed to fetch data for Pokemon ID: ${id}`);
+          return null;
+        }
 
-      if (!response.ok) {
-        return h.response({
-          error: 'Wasn’t able to fetch data from the API.',
-        }).code(400);
+        const data = await response.json();
+        const pokemonData = {
+          id: data.id,
+          name: data.name,
+          abilities: data.abilities.map((ability) => ability.ability.name),
+          weight: data.weight,
+          height: data.height,
+          types: data.types.map((type) => type.type.name),
+          image: data.sprites.front_default,
+        };
+
+        const result = await PokemonData.updateOne(
+          { id: pokemonData.id },
+          { $set: pokemonData },
+          { upsert: true }
+        );
+
+        if (result.modifiedCount === 0) {
+          console.info(`No changes were made to Pokemon with ID: ${id}.`);
+          return null; 
+        }
+
+        return pokemonData;
+      } catch (err) {
+        console.error(`Error saving Pokemon ID ${id}: ${err.message}`);
+        return null;
       }
-
-      const data = await response.json();
-      const pokemonData = {
-        id: data.id,
-        name: data.name,
-        abilities: data.abilities.map((ability) => ability.ability.name),
-        weight: data.weight,
-        height: data.height,
-        types: data.types.map((type) => type.type.name),
-        image: data.sprites.front_default,
-      };
-
-      await PokemonData.updateOne(
-        { id: pokemonData.id },
-        { $set: pokemonData },
-        { upsert: true }
-      );
-
-      return pokemonData;
     };
 
-    const promises = [];
-    for (let id = 1; id <= 100; id += 1) {
-      promises.push(savePokemon(id));
+    const batchSize = 20; 
+    const totalIds = 100;
+    for (let i = 0; i < totalIds; i += batchSize) {
+      const batchPromises = [];
+      for (let j = i + 1; j <= Math.min(i + batchSize, totalIds); j++) {
+        batchPromises.push(savePokemon(j));
+      }
+      await Promise.all(batchPromises);
     }
-    await Promise.all(promises);
 
     return h
       .response({
-        message: 'Fetched and saved Pokemon data for IDs 1 to 100.',
+        message: `Fetched and saved Pokemon data for IDs 1 to 100.`,
       })
       .code(200);
   } catch (error) {
